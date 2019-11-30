6fe6cd28c92c2b64936faa538cf16a15384265f0
dyy <2216502193@qq.com>
2019-11-30 15:32:28 +0800

    login through github

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-##Spring Boot实战练习
-##学习网站：
-【Spring Boot 实战】论坛项目【第一季】【修正版】
-
-[哔哩哔哩](https://www.bilibili.com/video/av65117012?p=1)
-
-##资料
+#Spring Boot实战练习   
+##学习网站：   
+【Spring Boot 实战】论坛项目【第一季】【修正版】   
+[哔哩哔哩](https://www.bilibili.com/video/av65117012?p=1)   
+##资料   
 [Spring文档](https://spring.io/guides)   
 [Bootstrap](https://v3.bootcss.com/getting-started/#download)   
 [Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
---
+++ src/main/java/com/spring/community/community/provider/GithubProvider.java
@@ -0,0 +1,55 @@
+package com.spring.community.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.spring.community.community.dto.AccessTokenDTO;
+import com.spring.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSON;
+
+import java.io.IOException;
+
+
+/**
+ * Okhttp3基本使用 https://www.jianshu.com/p/da4a806e599b
+ */
+@Component //可以不用new实例化对象
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post((okhttp3.RequestBody) body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
